42a4c82d63ecf6d08f689a5bffe36a137cfcc5cb
Akshat Vishwakarma <aaron.summers2797@gmail.com>
2019-05-03 14:05:05 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -55,13 +55,24 @@ end
 # get every test to pass before coding runner below #
 #####################################################
 
+# def runner
+#   welcome
+#   card_total = initial_round
+#   until card_total > 21 do
+#     hit?(card_total)
+#     display_card_total(card_total)
+#   end
+#   end_game(card_total)
+# end
 def runner
-  welcome
+  # code runner here
+  welcome  
   card_total = initial_round
-  until card_total > 21 do
-    hit?(card_total)
-    display_card_total(card_total)
+  while card_total < 21
+    card_total = hit?(card_total)
+    card_total = display_card_total(card_total)
   end
   end_game(card_total)
+
 end
     
